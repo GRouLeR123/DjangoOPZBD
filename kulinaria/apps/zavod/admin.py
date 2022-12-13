8dc6c7d7ca3a46cf2c86d94aa4a99c37ad3f3ba3
GRouLeR123 <m_anisimov_dima@mail.ru>
2022-12-13 14:35:00 +0300

    Initial commit

---
+++ kulinaria/apps/zavod/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Purchase)
+admin.site.register(Produced)
+admin.site.register(Menu)
+admin.site.register(Store)
+admin.site.register(Ingredients)
+admin.site.register(Suplier)
+
+# Register your models here.
